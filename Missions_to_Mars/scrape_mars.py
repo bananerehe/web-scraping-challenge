9cbec8d6c94d836e79e57247c9f8d3f34880546f
Justine <76693339+bananerehe@users.noreply.github.com>
2021-07-13 17:03:07 -0400

    Final commit

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -20,7 +20,7 @@ def scrape():
     news_p = news_titles.find(class_="article_teaser_body").text
 
     # Featured Image
-    url="https://www.jpl.nasa.gov/images?search=&category=Mars"
+    url = "https://www.jpl.nasa.gov/images?search=&category=Mars"
     browser.visit(url)
     time.sleep(1)
     browser.links.find_by_partial_text('Image').click()
@@ -30,33 +30,33 @@ def scrape():
     featured_image_url = soup.find('img', class_="BaseImage")['src']
 
     # Mars Facts
-    url="https://space-facts.com/mars/"
+    url = "https://space-facts.com/mars/"
     tables = pd.read_html(url)
-    df=tables[0]
-    df=df.rename(columns={0: "Description", 1: "Mars_Values"})
-    mars_facts_table=df.to_dict('records')
+    df = tables[0]
+    df = df.rename(columns={0: "Description", 1: "Mars_Values"})
+    mars_table = df.to_dict('records')
 
     # Mars Hemisphere
-    url="https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url)
     time.sleep(1)
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
-    titles=soup.find_all('h3')
-    titles[:]=(title.text for title in titles)
-    titles[:]=(title.split(" Enhanced")[0] for title in titles)
-    hemisphere_image_urls=[]
+    titles = soup.find_all('h3')
+    titles[:] = (title.text for title in titles)
+    titles[:] = (title.split(" Enhanced")[0] for title in titles)
+    hemisphere_image_urls = []
     for title in titles:
         browser.visit(url)
         browser.links.find_by_partial_text(title).click()
         time.sleep(1)
         html = browser.html
         soup = BeautifulSoup(html, 'html.parser')
-        img_url=soup.find('div',class_='downloads').ul.li.a['href']
+        img_url = soup.find('div',class_='downloads').ul.li.a['href']
         hemisphere_image_urls.append({"title": title, "img_url": img_url})
 
     # Organize data
-    mars_data={"news_title":news_title, "news_p":news_p,"featured_image_url":featured_image_url,"mars_facts_table":mars_facts_table,
+    mars_data = {"news_title":news_title, "news_p":news_p,"featured_image_url":featured_image_url,"mars_table":mars_table,
                 "hemisphere_image_urls":hemisphere_image_urls}
 
     browser.quit()
